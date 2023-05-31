3aa1462f92f7b89216432e6fb16a8ec58bf64e15
Alejandro Ravasio <alejandroravasio@gmail.com>
2023-05-31 16:44:56 -0300

    Initial Commit

---
+++ Tomatime/TomatimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatimeApp.swift
+//  Tomatime
+//
+//  Created by Alejandro Ravasio on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TomatimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
